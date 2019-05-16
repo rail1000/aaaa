44cf5b7dc9969fb5fcca1ed0efe80259f16943ac
rail1000 <rail1000@163.com>
2019-05-16 20:31:26 +0800

    week11

--- app/src/main/java/com/example/myapplication/MyListActivity.java
+++ app/src/main/java/com/example/myapplication/MyListActivity.java
@@ -2,22 +2,49 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 
-public class MyListActivity extends AppCompatActivity {
-    String data[] = {"a","b","c"};
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyListActivity extends AppCompatActivity implements AdapterView.OnItemClickListener,AdapterView.OnItemLongClickListener {
+    //String data[] = {"a","b","c"};
+    List<String> data = new ArrayList<String>();
+    String Tag = "mylist";
+    ArrayAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_list);
 
-        ListView listView = (ListView) findViewById(R.id.list);
-        String data[] ={"111","222"};
+        ListView listView = (ListView) findViewById(R.id.mylist);
+        for(int i =0 ;i<10;i++){
+            data.add("new"+i);
+        }
 
-
-        ListAdapter adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,data);
+         adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,data);
         listView.setAdapter(adapter);
+        listView.setEmptyView(findViewById(R.id.nodata));
+        listView.setOnItemClickListener(this);
+        listView.setOnItemLongClickListener(this);
+    }
+
+
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Log.i(Tag,"position"+position);
+        adapter.remove(parent.getItemAtPosition(position));
+        //通知改变
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+        return true;
     }
 }
